0a678f08d6905e355b66a31ce1a924af2d6a8cdd
SaranyaOP <saranyaop98@gmail.com>
2026-04-28 19:05:18 +0530

    Added project modified resume content

--- portfolio/src/data/data.jsx
+++ portfolio/src/data/data.jsx
@@ -2,11 +2,19 @@ import project_chatapp from '../assets/project_chatapp.png'
 import project_ai_chat_bot from '../assets/project_ai_chat_bot.png'
 import project_crud_app from '../assets/project_crud_app.png'
 import project_youtube_app from '../assets/project_youtube_app.png'
+import project_life_admin_ai from '../assets/project_life_admin_ai.png'
 
 // import project_chatapp from '/assets/project_app.png'
 // import project_chatapp from '/assets/project_app.png'
 
 export const projects = [
+  {
+    name: "Life admin AI",
+    tech: "React, Firestore, Gemini API, FastAPI, LLM integration",
+    github: 'https://github.com/SaranyaOP/Life-admin-ai',
+    live: 'https://life-admin-frontend-145379378917.us-central1.run.app/',
+    image: project_life_admin_ai
+  },
   {
     name: "Chat Application",
     tech: "React, Firebase, Tailwind",
@@ -21,13 +29,13 @@ export const projects = [
     live: 'https://ai-chatbot-2e109.web.app/',
     image:  project_ai_chat_bot
   },
-  {
-    name: "MERN CRUD App",
-    tech: "MongoDB, Express, React, Node",
-    github:'https://github.com/SaranyaOP/CRUD-MERN',
-    live: 'crud-mern-rho.vercel.app',
-    image: project_crud_app
-  },
+  // {
+  //   name: "MERN CRUD App",
+  //   tech: "MongoDB, Express, React, Node",
+  //   github:'https://github.com/SaranyaOP/CRUD-MERN',
+  //   live: 'crud-mern-rho.vercel.app',
+  //   image: project_crud_app
+  // },
   {
     name: "YouTube Clone",
     tech: "React, Hooks, Material UI",
